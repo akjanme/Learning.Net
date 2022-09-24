8d51092eac162d2c22cc23a8428abbce003cf417
akjanme <akjanme@hotmail.com>
2022-09-24 16:36:06 +0530

    properties and constructor

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,62 +6,11 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Car car = new Car();
-            car.CarDetails();
-
-
-
-
-
-
-            //Car car = new Car
-            //{
-            //    Color = "Red",
-            //    Model = 2021,
-            //    IsElectric = false,
-            //    Speed = "100"
-            //}; //object creation
-            //Console.WriteLine("Car Model is {0}, color is {1}, speed is {2} and is electric {3}", car.Model, car.Color, car.Speed, car.IsElectric);
-
-            //Car car1 = new Car
-            //{
-            //    Color = "White",
-            //    Model = 2020,
-            //    IsElectric = false,
-            //    Speed = "140"
-            //}; //object creation
-            //Console.WriteLine("Car Model is {0}, color is {1}, speed is {2} and is electric {3}", car1.Model, car1.Color, car1.Speed, car1.IsElectric);
-            //Console.WriteLine("Car Model is {0}, color is {1}, speed is {2} and is electric {3}", car.Model, car.Color, car.Speed, car.IsElectric);
-            Console.ReadKey();
+            Student student = new Student("Ramesh","Sharma",60);            
+            Console.WriteLine(student.Name);  
+            Console.WriteLine(student.Result); 
+            Console.ReadKey(); 
         }
     }
-    class Fruit
-    {
 
-    }
-    public class Car
-    {
-        private int Model;
-        private string Color;
-        private string Speed;
-        private bool IsElectric;
-
-        public void CarDetails()
-        {
-            Car car = new Car
-            {
-                Color = "Red",
-                Model = 2021,
-                IsElectric = false,
-                Speed = "100"
-            };
-            Console.WriteLine("Car Model is {0}, color is {1}, speed is {2} and is electric {3}", car.Model, car.Color, car.Speed, car.IsElectric);
-        }
-    }
-    public class Calculation
-    {
-        public int Num1;
-        public int Num2;
-        public int Total;
-    }
 }
---
+++ ConsoleApp/Student.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp
+{
+    public class Student
+    { 
+        public Student(string fname, string lname,int marks)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Marks = marks;
+        }  
+        private int ID { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        public string Name
+        {
+            get { return FirstName + " " + LastName; }
+        }
+        public string Result { get; set; }
+        private int marks;
+        private int Marks
+        {
+            get { return marks; }
+            set {
+                marks = value;
+                if (marks > 33)
+                {
+                    Result = "Pass";
+                }
+                else
+                {
+                    Result = "Fail";
+                }
+            }
+        } 
+    }
+}
